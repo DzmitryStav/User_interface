083494592c546945c5e91163b0721b314cf1378c
DzmitryStav <73167419+DzmitryStav@users.noreply.github.com>
2021-02-04 11:12:28 +0300

    Delete src/test directory

--- src/test/java/messages/AssertMessages.java
+++
@@ -1,5 +0,0 @@
-package messages;
-
-public class AssertMessages {
-    public static final String AUTHORIZATION_RESULT_ERROR = "Invalid result data after login";
-}
--- src/test/java/messages/ExpectedResults.java
+++
@@ -1,8 +0,0 @@
-package messages;
-
-public class ExpectedResults {
-    public static final String AUTHORIZATION_RESULTS = "{\n" +
-            "  \"authenticated\": true, \n" +
-            "  \"user\": \"user\"\n" +
-            "}";
-}
